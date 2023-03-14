0a52f892fabc71bcbe7db7dfd9a40f8fc415c4fa
pt <petarturukalo@gmail.com>
2023-03-14 18:26:15 +1100

    signal error codes

--- src/boot.c
+++ src/boot.c
@@ -1,10 +1,7 @@
-#include "led.h"  // TODO rm
 #include "int.h"
 #include "gic.h"
 #include "ic.h"
-#include "mmio.h"  // TODO rm
-#include "timer.h"  // TODO rm
-#include "debug.h"  // TODO rm
+#include "error.h"
 
 /*
  * Entry point to the C code, the function branched to when switching from 
@@ -15,13 +12,5 @@ void c_entry(void)
 	enable_interrupts();
 	/*gic_init();*/
 	ic_enable_interrupts();
-	sleep(1000);
-	led_init();
-	for (int i = 0; ; ++i) {
-		if (i%2 == 0) 
-			led_turn_on();
-		else
-			led_turn_off();
-		sleep(1000);
-	}
+	signal_error(ERR_PLACEHOLDER);
 }
---
+++ src/error.c
@@ -0,0 +1,28 @@
+#include "error.h"
+#include "led.h"
+#include "timer.h"
+
+#define SHORT_PAUSE_MS 370
+#define LONG_PAUSE_MS 2250
+
+void signal_error(enum error_code error)
+{
+	led_init();
+	do {
+		/* 
+		 * The LED is turned on while this bootloader program is being loaded.
+		 * Do a long pause before the first signal so it can be differentiated
+		 * from that load.
+		 */
+		sleep(LONG_PAUSE_MS);
+		for (int i = error; i--;) {
+			led_turn_on();
+			sleep(SHORT_PAUSE_MS);
+			led_turn_off();
+			/* Don't short pause when about to long pause. */
+			if (i) {
+				sleep(SHORT_PAUSE_MS);
+			}
+		}
+	} while (1);
+}
