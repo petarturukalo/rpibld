6ab67a22bcd7fb30f95e48c6bef1fe731a0988a1
pt <petarturukalo@gmail.com>
2023-06-07 09:40:23 +1000

    chg scratch reg r0 to non-scratch reg r4

--- bld/int.c
+++ bld/int.c
@@ -2,15 +2,16 @@
 
 void enable_interrupts(void)
 {
-	__asm__("mrs r0, cpsr\n\t"
-		"bic r0, #0x80\n\t"
-		"msr cpsr_c, r0");
+	// TODO define for 0x80?
+	__asm__("mrs r4, cpsr\n\t"
+		"bic r4, #0x80\n\t"
+		"msr cpsr_c, r4");
 }
 
 void disable_interrupts(void)
 {
-	__asm__("mrs r0, cpsr\n\t"
-		"orr r0, #0x80\n\t"
-		"msr cpsr_c, r0");
+	__asm__("mrs r4, cpsr\n\t"
+		"orr r4, #0x80\n\t"
+		"msr cpsr_c, r4");
 }
 
