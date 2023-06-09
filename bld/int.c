000ea0a8097a16233de849d8b594d21df0eee81a
pt <petarturukalo@gmail.com>
2023-06-09 18:46:47 +1000

    save and restore non scratch registers

--- bld/int.c
+++ bld/int.c
@@ -3,15 +3,19 @@
 void enable_interrupts(void)
 {
 	// TODO define for 0x80?
-	__asm__("mrs r4, cpsr\n\t"
+	__asm__("push {r4}\n\t"
+		"mrs r4, cpsr\n\t"
 		"bic r4, #0x80\n\t"
-		"msr cpsr_c, r4");
+		"msr cpsr_c, r4\n\t"
+		"pop {r4}");
 }
 
 void disable_interrupts(void)
 {
-	__asm__("mrs r4, cpsr\n\t"
+	__asm__("push {r4}\n\t"
+		"mrs r4, cpsr\n\t"
 		"orr r4, #0x80\n\t"
-		"msr cpsr_c, r4");
+		"msr cpsr_c, r4\n\t"
+		"pop {r4}");
 }
 
