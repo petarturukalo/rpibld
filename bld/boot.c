4a99786788aa1d2575fe2c06c5dc4fc8405ef99d
pt <petarturukalo@gmail.com>
2023-06-21 20:10:07 +1000

    break down long c_entry/main fn into parts

--- bld/boot.c
+++ bld/boot.c
@@ -61,6 +61,62 @@
 #define ZIMAGE_MAGIC_OFF 0x24
 
 /*
+ * Load MBR from first block on SD card into RAM.
+ * Return the start address of the MBR in RAM on success.
+ */
+static byte_t *load_mbr(void)
+{
+	byte_t *mbr_base_addr = heap_get_base_address();
+
+	serial_log("Loading MBR...");
+
+	if (!sd_read_blocks(mbr_base_addr, (void *)0, 1))
+		signal_error(ERROR_SD_READ);
+	if (!mbr_magic(mbr_base_addr)) {
+		serial_log("Error: couldn't find MBR on SD card: no MBR magic");
+		signal_error(ERROR_NO_MBR_MAGIC);
+	}
+	if (!mbr_partition_valid(IMAGE_PARTITION)) {
+		serial_log("Error: partition %u not a valid primary partition", 
+			   IMAGE_PARTITION);
+		signal_error(ERROR_INVALID_PARTITION);
+	}
+	serial_log("Successfully loaded and validated MBR");
+	return mbr_base_addr;
+}
+
+/*
+ * Load the start of the image from the image partition into RAM.
+ * Return the logical block address (LBA) of the image partition on success.
+ */
+static uint32_t load_image_head(byte_t *mbr_base_addr)
+{
+	uint32_t img_part_lba = mbr_get_partition_lba(mbr_base_addr, IMAGE_PARTITION);
+	uint32_t img_part_nblks = mbr_get_partition_nblks(mbr_base_addr, IMAGE_PARTITION);
+	/* Got all the data needed from MBR so safe to overwrite it in heap. */
+	struct image *img = heap_get_base_address();
+
+	serial_log("Loading image head from partition %u", IMAGE_PARTITION);
+
+	/* Read first block of image from image partition into RAM. */
+	if (!sd_read_blocks((byte_t *)img, (void *)img_part_lba, 1))
+		signal_error(ERROR_SD_READ);
+	if (img->magic != IMG_MAGIC) {
+		serial_log("Error: couldn't find image at start of partition %u: "
+			   "no image magic", IMAGE_PARTITION);
+		signal_error(ERROR_NO_IMAGE_MAGIC);
+	}
+	if (img->imgsz > img_part_nblks*SD_BLKSZ) {
+		serial_log("Error: image overflow: image size %u bytes is greater than size "
+			   "of image partition %u bytes", img->imgsz, img_part_nblks*SD_BLKSZ);
+		signal_error(ERROR_IMAGE_OVERFLOW);
+	}
+	serial_log("Successfully loaded and validated image head, "
+		   "image size %u bytes", img->imgsz);
+	return img_part_lba;
+}
+
+/*
  * Get the string name of an item which an item ID identifies.
  */
 static char *stritem(enum item_id id)
@@ -103,19 +159,52 @@ static struct item *load_item(enum item_id id, byte_t *ram_item_dest_addr,
 	return item;
 }
 
+/* 
+ * Load the kernel and device tree blob from the SD image into RAM. 
+ */
+static void load_image_items(uint32_t img_part_lba)
+{
+	/*
+	 * Below the size of the item not including its data is subtracted from the RAM 
+	 * address so that the start of the item's data is loaded to the RAM address.
+	 */
+	uint32_t item_lba = img_part_lba+1;
+	struct item *item = load_item(ITEM_ID_KERNEL, (byte_t *)(KERN_RAM_ADDR-sizeof(struct item)), 
+				      (void *)item_lba);
+	if (KERN_RAM_ADDR+item->itemsz >= DTB_RAM_ADDR) {
+		serial_log("Error: kernel size %u bytes overflows into device tree blob",
+			   item->itemsz);
+		signal_error(ERROR_KERN_OVERFLOW);
+	}
+	if (*(uint32_t *)(KERN_RAM_ADDR+ZIMAGE_MAGIC_OFF) != ZIMAGE_MAGIC) {
+		serial_log("Error: couldn't find kernel zImage magic");
+		signal_error(ERROR_IMAGE_CONTENTS);
+	}
+	serial_log("Successfully validated kernel");
+
+	item_lba += bytes_to_blocks(item->itemsz);
+	item = load_item(ITEM_ID_DEVICE_TREE_BLOB, (byte_t *)(DTB_RAM_ADDR-sizeof(struct item)), 
+			 (void *)item_lba);
+	if (bswap32(*(uint32_t *)DTB_RAM_ADDR) != DTB_MAGIC) {
+		serial_log("Error: couldn't find device tree blob magic");
+		signal_error(ERROR_IMAGE_CONTENTS);
+	}
+	serial_log("Successfully validated device tree blob");
+
+	/* Validate that the terminating item is there. */
+	item_lba += bytes_to_blocks(item->itemsz);
+	item = load_item(ITEM_ID_END, heap_get_base_address(), (void *)item_lba);
+}
+
 /*
  * Entry point to the C code, the function branched to when switching from 
  * the assembly init code to C.
  */
 void c_entry(void)
 {
-	// TODO break down the fn into parts? (break it down as per the serial log load and successes)
 	enum sd_init_error error;
 	byte_t *mbr_base_addr;
-	uint32_t img_part_lba, item_lba;
-	uint32_t img_part_nblks;
-	struct image *img;
-	struct item *item;
+	uint32_t img_part_lba;
 
 	enable_interrupts();
 	ic_enable_interrupts();
@@ -127,74 +216,9 @@ void c_entry(void)
 		serial_log("Failed to initialise SD");
 		signal_error(ERROR_SD_INIT);
 	}
-	serial_log("Loading MBR...");
-	mbr_base_addr = heap_get_base_address();
-	/* Read MBR from first block on SD card into RAM. */
-	if (!sd_read_blocks(mbr_base_addr, (void *)0, 1))
-		signal_error(ERROR_SD_READ);
-	if (!mbr_magic(mbr_base_addr)) {
-		serial_log("Error: couldn't find MBR on SD card: no MBR magic");
-		signal_error(ERROR_NO_MBR_MAGIC);
-	}
-	if (!mbr_partition_valid(IMAGE_PARTITION)) {
-		serial_log("Error: partition %u not a valid primary partition", 
-			   IMAGE_PARTITION);
-		signal_error(ERROR_INVALID_PARTITION);
-	}
-	serial_log("Successfully loaded and validated MBR");
-
-	img_part_lba = mbr_get_partition_lba(mbr_base_addr, IMAGE_PARTITION);
-	img_part_nblks = mbr_get_partition_nblks(mbr_base_addr, IMAGE_PARTITION);
-
-	serial_log("Loading image head from partition %u", IMAGE_PARTITION);
-	/* Got all the data needed from MBR so safe to overwrite it in heap. */
-	img = heap_get_base_address();
-	/* Read first block of image from image partition into RAM. */
-	if (!sd_read_blocks((byte_t *)img, (void *)img_part_lba, 1))
-		signal_error(ERROR_SD_READ);
-	if (img->magic != IMG_MAGIC) {
-		serial_log("Error: couldn't find image at start of partition %u: "
-			   "no image magic", IMAGE_PARTITION);
-		signal_error(ERROR_NO_IMAGE_MAGIC);
-	}
-	if (img->imgsz > img_part_nblks*SD_BLKSZ) {
-		serial_log("Error: image overflow: image size %u bytes is greater than size "
-			   "of image partition %u bytes", img->imgsz, img_part_nblks*SD_BLKSZ);
-		signal_error(ERROR_IMAGE_OVERFLOW);
-	}
-	serial_log("Successfully loaded and validated image head, "
-		   "image size %u bytes", img->imgsz);
-
-	/* 
-	 * Load kernel and device tree blob from SD image into RAM. 
-	 *
-	 * Below the size of the item not including its data is subtracted from the RAM 
-	 * address so that the start of the item's data is loaded to the RAM address.
-	 */
-	item_lba = img_part_lba+1;
-	item = load_item(ITEM_ID_KERNEL, (byte_t *)(KERN_RAM_ADDR-sizeof(struct item)), 
-			 (void *)item_lba);
-	if (KERN_RAM_ADDR+item->itemsz >= DTB_RAM_ADDR) {
-		serial_log("Error: kernel size %u bytes overflows into device tree blob",
-			   item->itemsz);
-		signal_error(ERROR_KERN_OVERFLOW);
-	}
-	if (*(uint32_t *)(KERN_RAM_ADDR+ZIMAGE_MAGIC_OFF) != ZIMAGE_MAGIC) {
-		serial_log("Error: couldn't find kernel zImage magic");
-		signal_error(ERROR_IMAGE_CONTENTS);
-	}
-	serial_log("Successfully validated kernel");
-	item_lba += bytes_to_blocks(item->itemsz);
-	item = load_item(ITEM_ID_DEVICE_TREE_BLOB, (byte_t *)(DTB_RAM_ADDR-sizeof(struct item)), 
-			 (void *)item_lba);
-	if (bswap32(*(uint32_t *)DTB_RAM_ADDR) != DTB_MAGIC) {
-		serial_log("Error: couldn't find device tree blob magic");
-		signal_error(ERROR_IMAGE_CONTENTS);
-	}
-	serial_log("Successfully validated device tree blob");
-	item_lba += bytes_to_blocks(item->itemsz);
-	/* Validate that the terminating item is there. */
-	item = load_item(ITEM_ID_END, heap_get_base_address(), (void *)item_lba);
+	mbr_base_addr = load_mbr();
+	img_part_lba = load_image_head(mbr_base_addr);
+	load_image_items(img_part_lba);
 
 	/* Reset CPU and peripherals to the state required to boot the kernel. */
 	// TODO test which of below is actually needed
